9b592046f5141db4d6be1615aea71edb11179e9b
Dhanya <dhanyak4580@gmail.com>
2025-04-02 19:43:23 +0530

    Fixed React Router issue and updated index.html

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,22 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import TopUsers from "./pages/TopUsers";
 import TrendingPosts from "./pages/TrendingPosts";
-import LiveFeed from "./pages/LiveFeed";
+import Feed from "./pages/LiveFeed";
 
-const App = () => {
+function App() {
   return (
     <Router>
       <Navbar />
-      <div className="container mx-auto p-4">
-        <Routes>
-          <Route path="/" element={<TopUsers />} />
-          <Route path="/trending" element={<TrendingPosts />} />
-          <Route path="/feed" element={<LiveFeed />} />
-        </Routes>
-      </div>
-  <div>
-    <h1 className="text-2xl font-bold">Social Media Analytics App</h1>
-  </div>
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="/top-users" element={<TopUsers />} />
+        <Route path="/trending-posts" element={<TrendingPosts />} />
+      </Routes>
     </Router>
   );
-};
+}
 
 export default App;
+
